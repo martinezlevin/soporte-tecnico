65a7b71fa16a6ece90f6068938a7466c0fd206a8
Guillermo Martinez Levin <gdmlevin@gmail.com>
2025-02-07 21:50:11 -0300

    Mejoras de interfaz

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Selecciona todas las cards
+const cards = document.querySelectorAll('.card6');
+
+// Añade un event listener a cada card
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        // Obtén el valor de data-target
+        const targetId = card.getAttribute('data-target');
+        // Encuentra la sección correspondiente usando el ID
+        const targetSection = document.getElementById(targetId);
+        // Desplázate a la sección con un efecto suave
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
