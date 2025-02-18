3ae1270bc10bf935ba6c2f815e5074040c91519d
Guillermo Martinez Levin <gdmlevin@gmail.com>
2025-02-18 16:32:09 -0300

    agrego pagina restante

--- js/script.js
+++ js/script.js
@@ -15,5 +15,23 @@ cards.forEach(card => {
     });
 });
 
+// Selecciona todas las cards con la clase 'card8'
+const cards8 = document.querySelectorAll('.card8');
+
+// Añade un event listener a cada card
+cards8.forEach(card => {
+    card.addEventListener('click', () => {
+        // Obtén el valor de data-target
+        const targetId = card.getAttribute('data-target');
+        // Encuentra la sección correspondiente usando el ID
+        const targetSection = document.getElementById(targetId);
+        // Desplázate a la sección con un efecto suave
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+
 // USO DE SCROLLREVEAL
 ScrollReveal().reveal('.headline');
